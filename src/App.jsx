5351d59460c28680cf914b58b663f5cb1539e6eb
nhacly <fg42oh@gmail.com>
2024-02-25 19:13:25 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import { useRef, useState } from 'react'
+import './App.css'
+import { DeleteSvg, SearchSvg } from './svg'
+
+function App() {
+  const cityRef = useRef()
+  const countryRef = useRef()
+  const [data, setData] = useState(null)
+  const [searchList, setSearchList] = useState([])
+
+  const getQuery = () => {
+    if (cityRef.current?.value && countryRef.current?.value) {
+      return cityRef.current.value + ',' + countryRef.current.value
+    }
+    if (cityRef.current?.value) {
+      return cityRef.current.value
+    }
+    if (countryRef.current?.value) {
+      return countryRef.current.value
+    }
+    return ''
+  }
+
+  const fetchData = async () => {
+    const q = getQuery()
+    if (!q) return
+    try {
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?${new URLSearchParams({
+        q,
+        appid: 'f717c4a03ccdb7b5482538125e0868fe'
+      })}`)
+      const json = await res.json()
+      setSearchList(prev => [...prev, { ...json, curTime: Date.now() }])
+      setData(json)
+    }
+    catch (e) { console.log(e) }
+  }
+
+  const clearData = () => {
+    cityRef.current.value = ''
+    countryRef.current.value = ''
+    setData(null)
+  }
+
+  return (
+    <>
+      <div>
+        <h2 style={{ textAlign: 'left' }}>Today's Weather</h2>
+        <hr />
+      </div>
+      <div>
+        <div style={{ display: 'flex', gap: '0px 20px' }}>
+          <label style={{ margin: 'auto', whiteSpace: 'nowrap' }}><span>City: </span><input type='text' ref={cityRef} /></label>
+          <label style={{ margin: 'auto', whiteSpace: 'nowrap' }}><span>Country: </span><input type='text' ref={countryRef} /></label>
+          <div style={{ display: 'flex', gap: '0px 10px' }}>
+            <div><button type='button' onClick={fetchData}><span>Search</span></button></div>
+            <div><button type='button' onClick={clearData}><span>Clear</span></button></div>
+          </div>
+        </div>
+        {data?.cod && data.cod === '404' && <div style={{ width: '100%', border: '1px solid red', padding: '10px' }}><span style={{ textAlign: 'left', color: 'red', fontWeight: 'bold' }}>{data.message}</span></div>}
+        <div style={{ display: 'grid', gridAutoColumns: '300px', padding: '20px 0', whiteSpace: 'nowrap' }}>
+          {data?.cod !== '404' && data && <div style={{ textAlign: 'left' }}>
+            <span style={{ fontWeight: 'lighter' }}>{data.name + ', ' + data.sys.country}</span>
+            <h1 style={{ margin: '1px' }}>{data.weather[0].main}</h1>
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+              <span style={{ fontWeight: 'lighter' }}>Description: </span><span style={{ fontWeight: 'bold' }}>{data.weather[0].description}</span>
+              <span style={{ fontWeight: 'lighter' }}>Temperature: </span><span style={{ fontWeight: 'bold' }}>{data.main.temp}</span>
+              <span style={{ fontWeight: 'lighter' }}>Humidity: </span><span style={{ fontWeight: 'bold' }}>{data.main.humidity}</span>
+              <span style={{ fontWeight: 'lighter' }}>Time: </span><span style={{ fontWeight: 'bold' }}>{new Date(+data?.dt * 1000).toLocaleDateString('en-US', { hour: '2-digit', minute: '2-digit' })}</span>
+            </div>
+          </div>}
+        </div>
+        <div>
+          <h2 style={{ textAlign: 'left' }}>Search History</h2>
+          <hr />
+          {
+            searchList?.length > 0 && searchList.map((item, index) => {
+              return <div key={index} style={{ display: 'flex', justifyContent: 'space-between', padding: '10px 0', borderBottom: '1px solid grey', fontWeight: '500' }}>
+                <span style={{ margin: 'auto 0' }}>{index + 1}. {item.name + ', ' + item.sys.country}</span>
+                <div style={{display: 'flex'}}>
+                  <span style={{ padding: '0 5px' }}>{new Date(item.curTime).toLocaleTimeString('en-US')}</span>
+                  <button style={{ height: '26px', padding: '1px'}} type='button' onClick={() => setData(item)}><SearchSvg/></button>
+                  <button style={{ height: '26px', padding: '1px'}} type='button' onClick={() => {
+                    setSearchList(prev => prev.filter((_, i) => i !== index))
+                  }}><DeleteSvg style={{margin: 'auto'}}/></button>
+                </div>
+              </div>
+            })
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/testData.js
@@ -0,0 +1,46 @@
+export const testData = {
+    "coord": {
+        "lon": 105.8412,
+        "lat": 21.0245
+    },
+    "weather": [
+        {
+            "id": 804,
+            "main": "Clouds",
+            "description": "overcast clouds",
+            "icon": "04d"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 287.15,
+        "feels_like": 286.36,
+        "temp_min": 287.15,
+        "temp_max": 287.15,
+        "pressure": 1018,
+        "humidity": 67,
+        "sea_level": 1018,
+        "grnd_level": 1016
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 1.64,
+        "deg": 80,
+        "gust": 1.97
+    },
+    "clouds": {
+        "all": 99
+    },
+    "dt": 1708849780,
+    "sys": {
+        "type": 1,
+        "id": 9308,
+        "country": "VN",
+        "sunrise": 1708816861,
+        "sunset": 1708858742
+    },
+    "timezone": 25200,
+    "id": 1581130,
+    "name": "Hanoi",
+    "cod": 200
+}
