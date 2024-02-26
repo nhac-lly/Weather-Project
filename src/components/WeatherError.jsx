bcc6a3c81db037dfd36dced9477de961a3dd8047
nhacly <fg42oh@gmail.com>
2024-02-26 14:25:23 +0700

    edit box

--- src/components/WeatherError.jsx
+++ src/components/WeatherError.jsx
@@ -1,4 +1,4 @@
-const WeatherError = ({ data }) => <><br /><div style={{ width: '100%', border: '1px solid red', padding: '0 20px' }}>
+const WeatherError = ({ data }) => <><br /><div style={{ border: '1px solid red', padding: '0 20px' }}>
     <span style={{ textAlign: 'left', color: 'red', fontWeight: 'bold' }}>{data.message}</span>
 </div></>
 
